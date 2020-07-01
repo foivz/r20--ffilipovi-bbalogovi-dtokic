57fc10bd4dc8c9e2e8b39706d792a2f66a6d7dd6
dtokic25 <danijel.tokic5@gmail.com>
2020-07-01 17:03:43 +0200

    Implementiran konstruktor za klasu Jelo

--- Software/Tokic/Jelo.cs
+++ Software/Tokic/Jelo.cs
@@ -8,8 +8,25 @@ namespace Vedrana
 	{
 		public jelo(string nnaziv, string ntip, List<int> sastojciId, List<double> kolicina)
 		{
-			throw new NotImplementedException();
-		}
+            using (var context = new Entities())
+            {
+                naziv = nnaziv;
+                tipJela = ntip;
+                
+                context.jeloes.Add(this);
+                context.SaveChanges();
+
+                for (int i = 0; i < sastojciId.Count(); i++)
+                {
+                    seSastoji temp = new seSastoji();
+                    temp.jeloId = this.jeloId;
+                    temp.proizvodId = sastojciId[i];
+                    temp.kolicina = kolicina[i];
+                    context.seSastojis.Add(temp);
+                }
+                context.SaveChanges();
+            }
+        }
 		public static List<jelo> PretraziJelaPoImenu(string naziv = "")
         {
 			throw new NotImplementedException();
