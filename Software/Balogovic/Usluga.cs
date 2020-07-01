3e09f5aed7c67742b1eaf4e91aeba7475d474542
bbalogovi <bbalogovi@foi.hr>
2020-07-01 16:39:00 +0200

    Napravljena klasa Usluga bez funkcionalnih metoda.

---
+++ Software/Balogovic/Usluga.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Vedrana
+{
+
+	public partial class usluga
+	{
+		public usluga(string nnaziv, double ncijena, string nopis, List<int> npopisProizvoda, List<double> kolicinaProizvoda)
+		{
+			throw new NotImplementedException();
+		}
+		public void PromjenaCijeneUsluge(double cijena)
+        {
+			throw new NotImplementedException();
+		}
+		public static List<usluga> PronadjiUsluguPoImenu(string naziv = "")
+        {
+			throw new NotImplementedException();
+		}
+	}
+
+}
+
