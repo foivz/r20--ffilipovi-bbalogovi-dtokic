6233eb0a8b2a3d0d335e7f9ee56fd368c847649a
dtokic25 <danijel.tokic5@gmail.com>
2020-07-01 16:35:31 +0200

    Dodana klasa Skladiste sa ne implementiranim metodama

---
+++ Software/Tokic/Skladiste.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vedrana.Tokic
+{
+	public class Skladiste
+	{
+		public static IEnumerable<object> DohvatiPopisSastojaka(string tip = "")
+        {
+			throw new NotImplementedException();
+        }
+		public static IEnumerable<object> PopisNarucenihStvari()
+        {
+			throw new NotImplementedException();
+        }
+	}
+}
