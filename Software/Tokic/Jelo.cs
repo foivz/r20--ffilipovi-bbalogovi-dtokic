6f0eb65b330b9ddc9223bbb79cf34e49502cd6ef
dtokic25 <danijel.tokic5@gmail.com>
2020-07-01 16:42:06 +0200

    Dodana klasa Jelo bez implementiranih metoda

---
+++ Software/Tokic/Jelo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vedrana
+{
+	public partial class jelo
+	{
+		public jelo(string nnaziv, string ntip, List<int> sastojciId, List<double> kolicina)
+		{
+			throw new NotImplementedException();
+		}
+		public static List<jelo> PretraziJelaPoImenu(string naziv = "")
+        {
+			throw new NotImplementedException();
+        }
+		public static List<jelo> PopisJelaPoTipu(string tip)
+        {
+			throw new NotImplementedException();
+        }
+	}
+}
