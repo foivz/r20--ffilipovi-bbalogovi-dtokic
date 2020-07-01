f3bb48ea6cf4efee67de4f75d58637c3769ca079
bbalogovi <bbalogovi@foi.hr>
2020-07-01 18:41:45 +0200

    Dodana forma EvidencijaKorisnika sa odgovarajućim elementima bez implementacije

---
+++ Software/Balogovic/EvidencijaKorisnika.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vedrana.Balogovic
+{
+    public partial class EvidencijaKorisnika : Form
+    {
+        public EvidencijaKorisnika()
+        {
+            InitializeComponent();
+        }
+    }
+}
