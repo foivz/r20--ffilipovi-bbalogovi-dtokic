e3ffe34c997476dcc245a3bedc77d51f6c7d5188
Filip F <filipovicf2105@gmail.com>
2020-07-01 15:52:41 +0200

    Added login functionality

--- Software/Filipovic/LogIn.cs
+++ Software/Filipovic/LogIn.cs
@@ -12,6 +12,7 @@ namespace Vedrana.Filipovic
 {
     public partial class LogIn : Form
     {
+        public static zaposlenik _zaposlenik = null;
         public LogIn()
         {
             InitializeComponent();
@@ -19,7 +20,28 @@ namespace Vedrana.Filipovic
 
         private void btnPrijava_Click(object sender, EventArgs e)
         {
+            try
+            {
+                _zaposlenik = zaposlenik.Autenfikacija(txtEmail.Text, txtSifra.Text);
 
+                if (_zaposlenik != null)
+                {
+                    txtSifra.Text = "";
+                    this.Hide();
+                    GlavniMeni forma = new GlavniMeni();
+                    forma.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    txtSifra.Text = "";
+                    MessageBox.Show("Krivi podaci");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnZaboravljenaLozinka_Click(object sender, EventArgs e)
