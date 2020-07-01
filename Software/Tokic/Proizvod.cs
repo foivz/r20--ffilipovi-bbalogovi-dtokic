fcf3e91196fc3247697dc1161dbe3296dc8e5a52
dtokic25 <danijel.tokic5@gmail.com>
2020-07-01 16:28:35 +0200

    Dodana klasa Proizvod s ne implementiranim konstruktorom

---
+++ Software/Tokic/Proizvod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vedrana
+{
+	public partial class proizvod
+	{
+		public proizvod(string nnaziv, string ntip, double kolicina=0)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
