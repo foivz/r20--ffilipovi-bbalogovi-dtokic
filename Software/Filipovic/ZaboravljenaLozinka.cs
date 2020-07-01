21da44702fd3f668897118a05b8d654f60acbf1f
Filip F <filipovicf2105@gmail.com>
2020-07-01 16:17:51 +0200

    Added functionality on zaboravljena lozinka

--- Software/Filipovic/ZaboravljenaLozinka.cs
+++ Software/Filipovic/ZaboravljenaLozinka.cs
@@ -16,5 +16,15 @@ namespace Vedrana.Filipovic
         {
             InitializeComponent();
         }
+
+        private void btnNovaLozinka_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
