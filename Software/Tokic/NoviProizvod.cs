a271b2039ba01e774898113f0fcf9adc0bb9a909
dtokic25 <danijel.tokic5@gmail.com>
2020-07-01 18:21:20 +0200

    Dodane funkcionalnosti za formu NoviProizvod

--- Software/Tokic/NoviProizvod.cs
+++ Software/Tokic/NoviProizvod.cs
@@ -16,5 +16,36 @@ namespace Vedrana.Tokic
         {
             InitializeComponent();
         }
+
+        private void NoviProizvod_Load(object sender, EventArgs e)
+        {
+            cbxTip.Items.Add("Sastojak");
+            cbxTip.Items.Add("Za njegu");
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtKolicina.Text == "")
+                {
+                    new proizvod(txtNaziv.Text, cbxTip.SelectedItem.ToString());
+                }
+                else
+                {
+                    new proizvod(txtNaziv.Text, cbxTip.SelectedItem.ToString(), double.Parse(txtKolicina.Text));
+                }
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
