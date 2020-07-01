68c742fb0e71bb5dbf65cd078991ff8815491d13
dtokic25 <danijel.tokic5@gmail.com>
2020-07-01 16:44:43 +0200

    Dodana klasa Jelovnik bez implementiranih metoda.

---
+++ Software/Tokic/Jelovnik.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Vedrana
+{
+	public partial class jelovnik
+	{
+		public jelovnik(jelo dorucak, jelo rucak, jelo uzina, jelo vecera, DateTime datum, bool poseban = false)
+		{
+			throw new NotImplementedException();
+		}
+		public void UrediJelovnik(jelo dorucak, jelo rucak, jelo uzina, jelo vecera)
+        {
+			throw new NotImplementedException();
+        }
+		public static Tuple<List<jelo>, List<jelo>> DanasnjiJelovnik()
+        {
+			throw new NotImplementedException();
+        }
+		public static Tuple<List<List<jelo>>, List<DateTime>, List<bool>> SviJelovnici()
+        {
+			throw new NotImplementedException();
+        }
+	}
+}
