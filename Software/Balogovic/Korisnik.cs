23be6538bc0c56ef7e285847111ead46bab4286c
bbalogovi <bbalogovi@foi.hr>
2020-07-01 18:25:52 +0200

    Implementiran konstruktor u klasi Korisnik

--- Software/Balogovic/Korisnik.cs
+++ Software/Balogovic/Korisnik.cs
@@ -12,7 +12,38 @@ namespace Vedrana
         public korisnik(string nime, string nprezime, string noib, string nadresa, DateTime ndatumRodjenja, string nkontakt, int nbrojSobe,
             List<imaUslugu> npopisUsluga, string nalergije, string nnapomene)
         {
-            throw new NotImplementedException();
+            ime = nime;
+            prezime = nprezime;
+            oib = noib;
+            adresa = nadresa;
+            datumRodjenja = ndatumRodjenja;
+            kontakt = nkontakt;
+            brojSobe = nbrojSobe;
+            alergije = nalergije;
+            napomene = nnapomene;
+            datumPocetka = DateTime.Now;
+            datumZavrsetka = null;
+
+            using (var context = new Entities())
+            {
+                osoba novaOsoba = new osoba();
+                novaOsoba.oib = noib;
+                novaOsoba.ime = nime;
+                novaOsoba.prezime = nprezime;
+                novaOsoba.adresa = nadresa;
+                novaOsoba.kontakt = nkontakt;
+                novaOsoba.datumRodjenja = ndatumRodjenja;
+                novaOsoba.datumPocetka = DateTime.Now;
+                novaOsoba.datumZavrsetka = null;
+                novaOsoba.koronaPozitivan = 0;
+                context.osobas.Add(novaOsoba);
+                context.korisniks.Add(this);
+                foreach (var novaUsluga in npopisUsluga)
+                {
+                    context.imaUslugus.Add(novaUsluga);
+                }
+                context.SaveChanges();
+            }
         }
         public static IEnumerable<object> PretragaKorisnika(bool bivsi, string ime = "", string prezime = "")
         {
