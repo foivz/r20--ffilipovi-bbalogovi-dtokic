e839b28edae2792e0b3d832bca8c0744f1b0379a
dtokic25 <danijel.tokic5@gmail.com>
2020-07-01 18:45:31 +0200

    Popravljeno skaliranje za izvjestaj statistike skladista

--- Software/Statistika/Statistika.cs
+++ Software/Statistika/Statistika.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,8 +20,21 @@ namespace Vedrana.Statistika
 
         private void Statistika_Load(object sender, EventArgs e)
         {
+            using (var context = new Entities())
+            {
+                var n = (from nx in context.seNarucujes
+                         select nx).ToList();
+                var p = (from px in context.proizvods
+                         select px).ToList();
+                var rds = new ReportDataSource("DataSet", n);
+                var rds2 = new ReportDataSource("DataSet2", p);
 
-            this.reportViewer1.RefreshReport();
+                this.reportViewer1.LocalReport.DataSources.Clear();
+                this.reportViewer1.LocalReport.DataSources.Add(rds);
+                this.reportViewer1.LocalReport.DataSources.Add(rds2);
+                this.reportViewer1.RefreshReport();
+                this.reportViewer1.RefreshReport();
+            }
         }
     }
 }
