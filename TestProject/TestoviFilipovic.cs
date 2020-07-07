683f599503c1d9a10bfa880ee990ad924c028d22
Filip F <filipovicf2105@gmail.com>
2020-07-08 00:45:31 +0200

    Added auth tests

---
+++ TestProject/TestoviFilipovic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vedrana;
+
+namespace TestProject
+{
+    [TestClass]
+    public class TestoviFilipovic
+    {
+        [TestMethod]
+        public void ProvjeraAutentifikacije()
+        {
+            //Arrange
+
+            //Act
+            var zap = zaposlenik.Autenfikacija("admin", "admin1");
+
+            //Assert
+            Assert.IsNotNull(zap);
+        }
+
+        [TestMethod]
+        public void PromjenaSifre()
+        {
+            //Arrange
+            var zap = zaposlenik.Autenfikacija("admin", "admin");
+
+            //Act
+            zap.PromjenaLozinke("admin1");
+            zap = null;
+            zap = zaposlenik.Autenfikacija("admin", "admin1");
+
+            //Assert
+            Assert.IsNotNull(zap);
+        }
+    }
+}
