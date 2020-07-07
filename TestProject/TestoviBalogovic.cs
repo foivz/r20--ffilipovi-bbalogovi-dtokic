c0afcd26c2722c391268bbdffb6fd9d5bd1f335f
bbalogovi <bbalogovi@foi.hr>
2020-07-07 18:24:01 +0200

    Dodani testovi dodavanja novog korisnika i promjena sobe korisnika

---
+++ TestProject/TestoviBalogovic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Vedrana;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class TestoviBalogovic
+    {
+        [TestMethod]
+        public void DodavanjeNovogKorisnika()
+        {
+            //Arrange
+            new korisnik("TestIme", "TestPrezime", "TestOIB1", "TestAdresa", DateTime.Now, "TestKontakt", 1,
+                new System.Collections.Generic.List<imaUslugu>(), "/", "/");
+
+            //Act
+            var kor = korisnik.PretragaKorisnika(false, "TestIme", "TestPrezime");
+
+            //Assert
+            Assert.IsNotNull(kor);
+        }
+        [TestMethod]
+        public void PromjenaSobeKorisniku()
+        {
+            //Arrange
+            var kor = new korisnik("TestIme2", "TestPrezime2", "TestOIB2", "TestAdresa2", DateTime.Now, "TestKontakt2", 1,
+                new System.Collections.Generic.List<imaUslugu>(), "/", "/");
+
+            //Act
+            kor.PromjeniSobu(2);
+
+            //Assert
+            Assert.AreEqual(2, kor.brojSobe);
+        }
+    }
+}
