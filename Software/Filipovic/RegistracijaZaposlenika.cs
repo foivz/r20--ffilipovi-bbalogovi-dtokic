83da7e2431f4d7e34a782f00f93e95dc9746da42
Filip F <filipovicf2105@gmail.com>
2020-07-07 10:34:52 +0200

    Added registracija zaposlenika form (design)

---
+++ Software/Filipovic/RegistracijaZaposlenika.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vedrana.Filipovic
+{
+    public partial class RegistracijaZaposlenika : Form
+    {
+        public RegistracijaZaposlenika()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistracijaZaposlenika_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistriraj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
