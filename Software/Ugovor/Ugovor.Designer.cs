6febc09d64e240398547bea31bf290be6d1d99ab
dtokic25 <danijel.tokic5@gmail.com>
2020-07-07 22:46:27 +0200

    Popravak izvjestaja ugovora

--- Software/Ugovor/Ugovor.Designer.cs
+++ Software/Ugovor/Ugovor.Designer.cs
@@ -34,17 +34,18 @@
             // reportViewer1
             // 
             this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Vedrana.Ugovor.UgovorReport.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(878, 746);
+            this.reportViewer1.Size = new System.Drawing.Size(1171, 833);
             this.reportViewer1.TabIndex = 0;
             // 
             // Ugovor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(878, 746);
+            this.ClientSize = new System.Drawing.Size(1171, 833);
             this.Controls.Add(this.reportViewer1);
             this.Name = "Ugovor";
             this.Text = "Ugovor";
